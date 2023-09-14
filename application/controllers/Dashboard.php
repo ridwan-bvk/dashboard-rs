8d3fd6df567743c8fcb877c894504edbdbec7a25
ridwan-bvk <readsmath@gmail.com>
2023-09-14 16:39:53 +0700

    tambahan lg
    go again

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,11 @@
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+    public function index()
+	{
+		$this->load->view('header');
+		$this->load->view('sidebar');
+		$this->load->view('footer');
+	}
+}
+
