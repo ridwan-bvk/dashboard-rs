4f5df7c3e23cabdb1d4cc6a232c8c7171a93d6e9
ridwan <readsmath@gmail.com>
2023-09-17 13:58:52 +0700

    update data curl by api
    get data by api

--- application/controllers/DashboardRekap.php
+++ application/controllers/DashboardRekap.php
@@ -3,17 +3,40 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class DashboardRekap extends CI_Controller
 {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('RekapModel');
-	}
+	// public function __construct()
+	// {
+	// 	parent::__construct();
+	// 	// $this->load->model('RekapModel');
+	// }
 
 	public function Index()
 	{
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://simrs.rsukotabanjar.co.id/ws-rsubanjar/dashboardpertgl',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => array('tanggal' => '2023-09-15', 'waktu' => 'rs'),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		$api_data = json_decode($response, true);
+		// echo $response;
+		// print_r($api_data);
+
+
 		$data['app_name'] = 'Dashboard App';
 		$data['title'] = 'Rekap Monitoring Pasien';
-		$data['data'] = $this->RekapModel->getdata('rekap_pendaftaran')->result();
+		$data['curl'] = $api_data;
+		// $data['data'] = $this->RekapModel->getdata('rekap_pendaftaran')->result();
 
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/sidebar', $data);
--- application/views/dashboard/rekap.php
+++ application/views/dashboard/rekap.php
@@ -1,29 +1,72 @@
 <div class="card">
+
     <!-- /.card-header -->
     <div class="card-body">
+        <!-- <form class="form-inline ">
+            <div class="input-group input-daterange">
+                <input type="text" class="form-control" value="2012-04-05">
+                <div class="input-group-addon"> to </div>
+                <input type="text" class="form-control" value="2012-04-19">
+            </div>
+        </form> -->
         <table id="example1" class="table table-bordered table-striped">
             <thead>
                 <tr class="text-center">
                     <th>No</th>
                     <th>Kode Booking</th>
-                    <th>Platform(s)</th>
-                    <th>Engine version</th>
+                    <th>waktu tunggu admisi</th>
+                    <th>Rata -rata akhir waktu tunggu poli/mulai waktu layan polin</th>
                     <th>CSS grade</th>
                 </tr>
             </thead>
-            <?php $no = 1;
-            foreach ($data as $dt) : ?>
-                <tbody>
-                    <tr>
-                        <td><?= $no++ ?></td>
-                        <td><?= $dt->kd_booking ?>
-                        </td>
-                        <td>Win 95+</td>
-                        <td> 4</td>
-                        <td>X</td>
-                    </tr>
-                </tbody>
-            <?php endforeach ?>
+            <?php
+            $no = 1;
+            foreach ($curl as $data1) :
+                foreach ($data1 as $data2) :
+                    if (is_array($data2) || is_object($data2)) {
+                        foreach ($data2 as $value) :
+            ?>
+                            <tbody>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $value['kdppk'] ?></td>
+                                    <td><?php
+                                        $input = $value['waktu_task1'] / 1000;
+                                        $date = strtotime($input);
+                                        echo date('hh:mm:ss', $date);
+
+
+
+                                        ?></td>
+                                    <td><?= $value['avg_waktu_task4'] ?></td>
+                                </tr>
+                            </tbody>
+            <?php
+                        endforeach;
+                    }
+                endforeach;
+            endforeach;
+            ?>
         </table>
     </div>
 </div>
+
+<!-- <script>
+    var url = 'https://simrs.rsukotabanjar.co.id/ws-rsubanjar/dashboardpertgl';
+    var formData = new FormData();
+    formData.append('tanggal', '2023-09-15');
+    formData.append('waktu', 'rs');
+
+
+    fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(body) {
+            // console.log(body);
+            document.cookie = 'data' + body;
+        });
+</script> -->
